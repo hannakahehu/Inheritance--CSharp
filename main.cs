413cc2e77bf7a57454cbc725a60d349cc7789be7
Hanna kahehu <39971210+hannakahehu@users.noreply.github.com>
2021-09-28 10:24:41 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+
+     Menu menu= new Menu();
+     menu.foodMenu();
+
+     menu.BeverageMenu();
+    //Console.WriteLine ("Hello World");
+  }
+}
---
+++ menu.cs
@@ -0,0 +1,14 @@
+using System;
+ 
+ class Menu {
+   public void  BeverageMenu(){
+   Console.WriteLine("Tea  coffee  lemon Tea ");
+   }
+     public void  foodMenu(){
+   Console.WriteLine(" chicken, Fish   ");
+   }
+     public void  DesertMenu(){
+   Console.WriteLine(" cake, ice Cream ");
+   }
+}
+
